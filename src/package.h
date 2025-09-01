689511a6a2c8ceea15904bba3c10f149f806b20f
Julijan Andjelic <julijan.andjelic@gmail.com>
2025-09-01 18:55:24 +0200

    added comments for Package methods

--- src/package.h
+++ src/package.h
@@ -29,17 +29,40 @@ public:
 		bool managed,
 		int registeredAt
 	);
+
+	// create a project (managed package)
 	static void create(const char* const name);
+
+	// return all registered packages
 	static std::vector<Package> packages();
+
+	// return all registered packages as boost::json:array
 	static boost::json::array packagesJSON();
+
+	// check if package with given name exists
 	static bool packageExists(const char* const name);
+
+	// if given package exists, returns std::variant holding Package instance
 	static MaybePackage get(const char* const name);
+
+	// if given package exists, returns std::variant holding boost::json::object
 	static MaybePackageJSON getJSON(const char* const name);
+
+	// return package dependencies
 	static std::vector<Package> getDependencies(const char* const name);
+
+	// convert from enum PackageType to string
 	static std::string typeToString(PackageType t);
+
+	// convert string to enum PackageType
 	static PackageType typeFromString(const char* const t);
-	static Package fromJSON(boost::json::value data);
+
+	// given a boost::json::object returns instance of Package
+	static Package fromJSON(boost::json::object data);
+
+	// given a Package instance returns boost::json::object
 	static boost::json::object toJSON(Package& pkg);
 private:
+	// prompt user to select a package type
 	static std::string promptType();
 };
