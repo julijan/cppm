07e31708ceacaa1e5b6cca5b7f8bf4f3a059acef
Julijan Andjelic <julijan.andjelic@gmail.com>
2025-09-25 19:45:13 +0200

    re-generated premake

--- premake5.lua
+++ premake5.lua
@@ -2,20 +2,30 @@ workspace "cppm"
 	configurations { "Debug", "Release" }
 
 project "cppm"
-	kind "ConsoleApp"
 	language "C++"
+	kind "ConsoleApp"
 	cppdialect "C++20"
 	architecture "x64"
 	targetdir "bin/%{cfg.buildcfg}"
-	includedirs { "/home/julijan/.vcpkg/installed/x64-linux/include" }
-	libdirs { "/home/julijan/.vcpkg/installed/x64-linux/lib" }
-	links { "boost_json" }
 	files { "./src/**.h", "./src/**.cpp" }
+	includedirs { "./includes/src/**", "./includes/uses/src/**" }
+	libdirs { "./includes/lib/**", "./includes/uses/lib/**" }
+	links {
+		"print-nice",
+		"text-styled",
+		"boost_json",
+		"boost_json",
+		"boost_container",
+		"boost_container",
+		"boost_system",
+		"boost_system",
+		"fmt",
+		"fmtd"
+	}
 
 	filter "configurations:Debug"
 		defines { "DEBUG" }
 		symbols "On"
-
 	filter "configurations:Release"
 		defines { "NDEBUG" }
 		optimize "On"
